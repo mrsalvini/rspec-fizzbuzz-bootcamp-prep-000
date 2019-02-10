305103a26f6ab1d27fdf69a5a44d0b95b75c3ca8
Diogo Costa <dsgmcosta@gmail.com>
2019-02-10 21:43:16 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def printer(n)
+def fizzbuzz(n)
   if n % 3 == 0 && n % 5 == 0
     puts "FizzBuzz"
   elsif n % 3 == 0
@@ -11,11 +11,4 @@ def printer(n)
   else
     puts "#{n}"
   end
-end
-
-counter = 0
-
-100.times do
-  printer(counter)
-  counter += 1
 end
