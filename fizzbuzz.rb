22b9c9180f42b4f76dd17e0c1ed0721c9b79531f
Diogo Costa <dsgmcosta@gmail.com>
2019-02-10 21:58:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,12 +3,12 @@
 
 def fizzbuzz(n)
   if n % 3 == 0 && n % 5 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif n % 3 == 0
-    puts "Fizz"
+    "Fizz"
   elsif n % 5 == 0
-    puts "Buzz"
+    "Buzz"
   else
-    puts "#{n}"
+    nil
   end
 end
